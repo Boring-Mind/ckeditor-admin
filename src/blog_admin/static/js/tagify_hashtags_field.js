bb2138b2d718b342eacf355b083f01db5d52a39a
BoringMind <super.d3280@ya.ru>
2020-06-16 08:01:40 +0300

    feet: changed hashtag field placeholder to more helpful

--- src/blog_admin/static/js/tagify_hashtags_field.js
+++ src/blog_admin/static/js/tagify_hashtags_field.js
@@ -1,6 +1,6 @@
 var hashtags_input = document.querySelector("input#hashtags_input")
 tagify = new Tagify(hashtags_input, {
     maxTags: 5,
-    placeholder: 'Add a tag...',
+    placeholder: "E.g. 'Machine Learning' or 'CI/CD'",
     pattern: /^[a-zA-Z0-9 \/-]{0,30}$/
 })
